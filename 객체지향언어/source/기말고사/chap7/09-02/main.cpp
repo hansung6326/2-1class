26fb3f860bdbb0b0d1af9c5138d8ba82f8d667ea
hansung6326 <hansung6326@gmail.com>
2026-06-02 19:25:16 +0900

    09-02

---
+++ 객체지향언어/source/기말고사/chap7/09-02/main.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+class Converter {
+private:
+	string srcUnit, destUnit;
+protected:
+	double ratio;
+	virtual double convert(double src) {
+		return 0.0;
+	}
+public:
+	Converter(string sUnit, string dUnit, double ratio) {
+		srcUnit = sUnit;
+		destUnit = dUnit;
+		this->ratio = ratio;
+	}
+	void run() {
+		cout << srcUnit << "을  " << destUnit << "로 바꿉니다. ";
+		cout << srcUnit << "을 입력하세요>>";
+		double src;
+		cin >> src;
+		cout << convert(src) << destUnit << "입니다." << endl;
+	}
+};
+
+class Inch2Centimeter : public Converter {
+public:
+	Inch2Centimeter(string sUnit, string dUnit, double ratio) : Converter(sUnit, dUnit, ratio) {
+
+	}
+	double convert(double src) {
+		return src * (int)ratio;
+	}
+};
+
+int main() {
+	Inch2Centimeter i2m("inch", "cm", 2.54);
+	i2m.run();
+}
