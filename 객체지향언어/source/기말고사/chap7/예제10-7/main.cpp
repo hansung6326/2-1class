4952c11ea98af7b56ff8e61c0f7b6cb4f56f4eb9
hansung6326 <hansung6326@gmail.com>
2026-06-02 21:27:38 +0900

    예제10-7

---
+++ 객체지향언어/source/기말고사/chap7/예제10-7/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+	vector<string> v;
+	string name;
+
+	cout << "이름을 5개 입력하라" << endl;
+	for (int i = 0; i < 5; i++) {
+		cin >> name;
+		v.push_back(name);
+	}
+	name = v.at(0);
+	for (int i = 1; i < v.size(); i++) {
+		if (v[i] > name)
+			name = v[i];
+	}
+	cout << "사전에서 가장 뒤에 나오는 이름은 " << name << endl;
+}
