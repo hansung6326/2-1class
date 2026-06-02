8a5be46c46108c7c7cafeccd811b024edaf84efd
hansung6326 <hansung6326@gmail.com>
2026-06-02 21:20:16 +0900

    예제10-5

---
+++ 객체지향언어/source/기말고사/chap7/예제10-5/main.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+using namespace std;
+
+template <class T>
+class MyStack {
+	int tos;
+	T data[100];
+public:
+	MyStack();
+	void push(T n);
+	T pop();
+};
+
+template <class T>
+MyStack<T>::MyStack() {
+	tos = -1;
+}
+
+template <class T>
+void MyStack<T>::push(T n) {
+	if (tos >= 99)
+		return;
+	data[++tos] = n;
+}
+
+template <class T>
+T MyStack<T>::pop() {
+	if (tos < 0)
+		return 0;
+	return data[tos--];
+}
+
+int main() {
+	MyStack<int> iStack;
+	iStack.push(3);
+	cout << iStack.pop() << endl;
+
+	MyStack<double> dStack;
+	dStack.push(3.5);
+	cout << dStack.pop() << endl;
+
+	MyStack<char>* p = new MyStack<char>();
+	p->push('a');
+	cout << p->pop() << endl;
+	delete p;
+}
