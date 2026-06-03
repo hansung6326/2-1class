aa1a58f0bc823658ffc467426e15bb1fa9d7c2c7
hansung6326 <hansung6326@gmail.com>
2026-06-03 17:07:13 +0900

    10-01

---
+++ 객체지향언어/source/기말고사/chap7/10-01/main.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+template<class T>
+T biggest(T a[], int size) {
+	T big = a[0];
+	for (int i = 1; i < size; i++) {
+		if (a[i] > big)
+			big = a[i];
+	}
+	return big;
+}
+
+int main() {
+	int x[] = { 1, 10, 100, 5, 4 };
+	cout << biggest(x, 5) << endl;
+
+	double y[] = { 3.5, 20.7, 6.2, 5.4 };
+	cout << biggest(y, 4) << endl;
+}
