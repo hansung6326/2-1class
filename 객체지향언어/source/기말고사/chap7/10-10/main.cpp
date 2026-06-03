decd544ba33f38af428306b3be31f5e608c9b45f
hansung6326 <hansung6326@gmail.com>
2026-06-03 23:29:34 +0900

    10-10

---
+++ 객체지향언어/source/기말고사/chap7/10-10/main.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int main() {
+	map<string, int> voteMap;
+	string name;
+	bool stop = false;
+	while (true) {
+		cout << "이름 3개>>";
+		for (int i = 0; i < 3; i++) {
+			cin >> name;
+			if (name == "그만") {
+				stop = true;
+				break;
+			}
+			if (voteMap.find(name) == voteMap.end())
+				voteMap.insert(make_pair(name, 1));
+			else {
+				voteMap[name]++;
+			}
+		}
+		if (stop)
+			break;
+	}
+	
+	map<string, int>::iterator it;
+	map<string, int>::iterator bigIt = voteMap.begin();
+
+	for (it = voteMap.begin(); it != voteMap.end(); it++) {
+		cout << it->first << ":" << it->second << ", ";
+		if (it->second > bigIt->second) {
+			bigIt = it;
+		}
+	}
+	cout << endl;
+	cout << bigIt->first << "님이 회장으로 선출되었습니다." << endl;
+
+
+}
