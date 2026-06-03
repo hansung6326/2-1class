fe41b8805a8416b155ae460c56cffc64252ceaf7
hansung6326 <hansung6326@gmail.com>
2026-06-03 17:10:28 +0900

    10-02

---
+++ 객체지향언어/source/기말고사/chap7/10-02/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+template <class T>
+void print(T a, T b) {
+	for (T i = a; i <= b; i++) {
+		cout << i << ' ';
+	}
+	cout << endl;
+}
+
+int main() {
+	print(500, 507);
+	print(3.5, 6.5);
+	print('b', 'y');
+}
