80617109c1ff71797f805424b1bd95ddd5af07ef
hansung6326 <hansung6326@gmail.com>
2026-06-03 22:43:32 +0900

    10-09

---
+++ 객체지향언어/source/기말고사/chap7/10-09/main.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+	map<string, int> m;
+	m.insert(make_pair("짜장면", 5000));
+	m.insert(make_pair("짬뽕", 6000));
+	m.insert(make_pair("볶음밥", 8000));
+	m.insert(make_pair("탕수육", 25000));
+
+	string menu;
+	int num;
+	while (true) {
+		cout << "주문>>";
+		cin >> menu;
+		if (menu == "그만")
+			break;
+		cin >> num;
+		if (m.find(menu) == m.end()) {
+			cout << "없는 메뉴입니다." << endl;
+		}
+		else {
+			cout << "가격은 " << m[menu] * num << "원입니다." << endl;
+		}
+			
+	}
+}
