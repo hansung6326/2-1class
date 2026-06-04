d6216309799df53c394d091f6a1dea6345432769
hansung6326 <hansung6326@gmail.com>
2026-06-04 19:58:02 +0900

    11-04

---
+++ 객체지향언어/source/기말고사/chap7/11-04/main.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+	char str[80], bigStr[80] = "";
+	int no = 1, bigNo = 1;
+
+
+	cout << "최대 79글자까지 입력 가능합니다." << endl;
+	while (true) {
+		cout << no << ":\t";
+		cin.getline(str, 80);
+		if (cin.eof())
+			break;
+		if (cin.gcount() > strlen(bigStr) + 1) {
+			strcpy(bigStr, str);
+			bigNo = no;
+		}
+		no++;
+	}
+	cout << "가장 긴 문장은 라인 " << bigNo << ":" << bigStr << endl;
+}
