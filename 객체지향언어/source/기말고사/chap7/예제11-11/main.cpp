78416d2c87648f7e4e0b683690ca5cdab360e1dc
hansung6326 <hansung6326@gmail.com>
2026-06-04 17:43:50 +0900

    예제11-10

---
+++ 객체지향언어/source/기말고사/chap7/예제11-11/main.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+class Point {
+	int x, y;
+public:
+	Point(int x = 0, int y = 0) {
+		this->x = x;
+		this->y = y;
+	}
+	friend istream& operator >>(istream& stream, Point& p);
+	friend ostream& operator <<(ostream& stream, Point p);
+};
+
+istream& operator >>(istream& stream, Point& p) {
+	cout << "x 좌표>>";
+	stream >> p.x;
+	cout << "y 좌표>>";
+	stream >> p.y;
+	return stream;
+}
+
+ostream& operator <<(ostream& stream, Point p) {
+	stream << "(" << p.x << "," << p.y << ")" << endl;
+	return stream;
+}
+
+int main() {
+	Point p;
+	cin >> p;
+	cout << p;
+}
---
+++ 객체지향언어/source/기말고사/chap7/예제11-5/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int n = 30;
+
+	cout << n << endl;
+
+	cout.unsetf(ios::dec);
+	cout.setf(ios::hex);
+	cout << n << endl;
+
+	cout.setf(ios::showbase);
+	cout << n << endl;
+
+	cout.setf(ios::uppercase);
+	cout << n << endl;
+
+	cout.setf(ios::dec | ios::showpoint);
+	cout << 23.5 << endl;
+
+	cout.setf(ios::scientific);
+	cout << 23.5 << endl;
+
+	cout.setf(ios::showpos);
+	cout << 23.5 << endl;
+}
