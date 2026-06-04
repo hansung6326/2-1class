a293a0fb453c50dfbc3bdd01084b4e692ead10a3
hansung6326 <hansung6326@gmail.com>
2026-06-04 19:35:23 +0900

    11-02

---
+++ 객체지향언어/source/기말고사/chap7/11-02/main.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+int main() {
+	cout << "입력하세요>>";
+	char ch;
+	int c = 0;
+	while (true) {
+		cin.get(ch);
+		if (cin.eof())
+			break;
+		if (ch >= '0' && ch <= '9')
+			c++;
+		if (ch == '\n')
+			break;
+	}
+	cout << "숫자는 모두 " << c << "개 입력됨" << endl;
+}
