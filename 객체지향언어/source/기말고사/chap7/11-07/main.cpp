c313de463d1e4556c89e8b71613fc4500e901407
hansung6326 <hansung6326@gmail.com>
2026-06-05 23:42:30 +0900

    11-07

---
+++ 객체지향언어/source/기말고사/chap7/11-07/main.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Phone {
+	string name, telnum, address;
+public:
+	Phone(string name = "", string telnum = "", string address = "") {
+		this->name = name;
+		this->telnum = telnum;
+		this->address = address;
+	}
+	friend istream& operator >>(istream& ist, Phone& p);
+	friend ostream& operator <<(ostream& ost, Phone p);
+};
+
+istream& operator >>(istream& ist, Phone& p) {
+	cout << "이름:";
+	getline(ist, p.name);
+	cout << "전화번호:";
+	getline(ist, p.telnum);
+	cout << "주소:";
+	getline(ist, p.address);
+	return ist;
+}
+
+ostream& operator <<(ostream& ost, Phone p) {
+	ost << "(" << p.name << "," << p.telnum << "," << p.address << ")" << endl;
+	return ost;
+}
+
+int main() {
+	Phone queen, prince;
+	cin >> queen >> prince;
+	cout << queen << endl << prince << endl;
+
+}
