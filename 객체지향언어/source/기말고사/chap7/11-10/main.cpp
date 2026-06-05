02fed9c430a04688275f099f15a0e26eb0f95d69
hansung6326 <hansung6326@gmail.com>
2026-06-06 00:07:06 +0900

    11-10

---
+++ 객체지향언어/source/기말고사/chap7/11-10/main.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+istream& prompt(istream& ist) {
+	cout << "암호>>";
+	return ist;
+}
+
+int main() {
+	string password;
+	while (true) {
+		cin >> prompt >> password;
+		if (password == "C++") {
+			cout << "login success!!" << endl;
+			break;
+		}
+		cout << "login fail. try again!!" << endl;
+	}
+}
