a5872345465e359ee11ef0d15dbe45331712fead
hansung6326 <hansung6326@gmail.com>
2026-06-06 00:04:35 +0900

    11-09

---
+++ 객체지향언어/source/기말고사/chap7/11-09/main.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+istream& promptName(istream& ist) {
+	cout << "이름 입력>>";
+	return ist;
+}
+istream& promptAge(istream& ist) {
+	cout << "나이 입력>>";
+	return ist;
+}
+
+int main() {
+	string name;
+	int age;
+	cin >> promptName >> name;
+	cin >> promptAge >> age;
+	cout << "이름은 " << name << ", 나이는 " << age << endl;
+}
