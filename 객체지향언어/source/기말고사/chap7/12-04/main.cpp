9ba5fc1c773561290d9f0d154240a130a844828e
hansung6326 <hansung6326@gmail.com>
2026-06-06 18:07:57 +0900

    12-04

---
+++ 객체지향언어/source/기말고사/chap7/12-04/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+	ifstream fin("c:\\windows\\system.ini");
+	if (!fin) {
+		cout << "파일 열기 실패" << endl;
+		return 0;
+	}
+	ofstream fout("c:\\temp\\system.txt");
+	if (!fout) {
+		cout << "파일 열기 실패" << endl;
+		return 0;
+	}
+
+	int c;
+	while ((c = fin.get()) != EOF) {
+		fout << (char)toupper(c);
+	}
+
+	fin.close();
+	fout.close();
+
+}
