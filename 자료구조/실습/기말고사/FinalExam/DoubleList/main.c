1283e2905e5409c1d74b7dd1924554cbe9563f09
hansung6326 <hansung6326@gmail.com>
2026-06-08 03:18:44 +0900

    이중 연결 리스트

---
+++ 자료구조/실습/기말고사/FinalExam/DoubleList/main.c
@@ -0,0 +1,54 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+typedef int element;
+
+typedef struct DlistNode {
+	element data;
+	struct DlistNode* llink;
+	struct DlistNode* rlink;
+}DlistNode;
+
+void init(DlistNode* head) {
+	head->llink = head;
+	head->rlink = head;
+}
+
+void insert(DlistNode* before, element data) {
+	DlistNode* newnode = (DlistNode*)malloc(sizeof(DlistNode));
+	newnode->data = data;
+	newnode->llink = before;
+	newnode->rlink = before->rlink;
+	before->rlink->llink = newnode;
+	before->rlink = newnode;
+}
+
+void delete(DlistNode* head, DlistNode* removed) {
+	if (removed == head)
+		return;
+	removed->llink->rlink = removed->rlink;
+	removed->rlink->llink = removed->llink;
+	free(removed);
+}
+
+void print(DlistNode* head) {
+	for (DlistNode* p = head->rlink; p != head; p = p->rlink) {
+		printf("<- | |%d| | ->", p->data);
+	}
+	printf("\n");
+}
+
+int main() {
+	DlistNode* head = (DlistNode*)malloc(sizeof(DlistNode));
+	init(head);
+	for (int i = 0; i < 5; i++) {
+		insert(head, i);
+		print(head);
+	}
+	printf("삭제\n");
+	for (int i = 0; i < 5; i++) {
+		print(head);
+		delete(head, head->rlink);
+	}
+	free(head);
+}
