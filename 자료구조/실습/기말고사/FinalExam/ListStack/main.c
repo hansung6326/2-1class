32bd0ef009a7aee38270184415c3ddccb23dc89d
hansung6326 <hansung6326@gmail.com>
2026-06-08 03:37:04 +0900

    연결 리스트로 구현한 스택

---
+++ 자료구조/실습/기말고사/FinalExam/ListStack/main.c
@@ -0,0 +1,62 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+typedef int element;
+
+typedef struct StackNode{
+	element data;
+	struct StackNode* link;
+}StackNode;
+
+typedef struct {
+	StackNode* top;
+}LinkedStackType;
+
+void init(LinkedStackType* s) {
+	s->top = NULL;
+}
+
+int is_empty(LinkedStackType* s) {
+	return s->top == NULL;
+}
+
+int is_full(LinkedStackType* s) {
+	return 0;
+}
+
+void push(LinkedStackType* s, element data) {
+	StackNode* temp = (StackNode*)malloc(sizeof(StackNode));
+	temp->data = data;
+	temp->link = s->top;
+	s->top = temp;
+}
+
+element pop(LinkedStackType* s) {
+	if (is_empty(s)) {
+		printf("stack is empty\n");
+		exit(1);
+	}
+	StackNode* temp = s->top;
+	element item = temp->data;
+	s->top = s->top->link;
+	free(temp);
+	return item;
+}
+
+void print_stack(LinkedStackType* s) {
+	for (StackNode* p = s->top; p != NULL; p = p->link) {
+		printf("%d->", p->data);
+	}
+	printf("NULL \n");
+}
+
+int main() {
+	LinkedStackType s;
+	init(&s);
+	push(&s, 1);	 print_stack(&s);
+	push(&s, 2);	 print_stack(&s);
+	push(&s, 3);	 print_stack(&s);
+	pop(&s);		 print_stack(&s);
+	pop(&s);		 print_stack(&s);
+	pop(&s);		 print_stack(&s);
+}
