c054de508588248756d9860c848200866dfa4e25
hansung6326 <hansung6326@gmail.com>
2026-06-08 18:53:38 +0900

    8-6 문제 수정

---
+++ 객체지향언어/source/기말고사/chap7/test/main.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+using namespace std;
+
+class Schedule {
+protected:
+	int count;
+	int* jobs;
+	int curLoc;
+public:
+	Schedule(int curloc, int n[], int count) {
+		this->curLoc = curloc;
+		this->count = count;
+		jobs = new int[count]();
+		for (int i = 0; i < count; i++) {
+			jobs[i] = n[i];
+		}
+	}
+	~Schedule() {
+		delete[] jobs;
+	}
+	void run() {
+		int sum = 0;
+
+		cout << "현재 위치는 " << getCurLoc() << "동: 배달 시작 ..." << getCurLoc();
+		for (int i = 0; i < count; i++) {
+			sum += abs(getCurLoc() - jobs[i]);
+			curLoc = jobs[i];
+			cout << " -> " << getCurLoc();
+		}
+		cout << endl;
+		cout << "전체 움직인 거리는 " << sum << ", ";
+		cout << "현재 위치는 " << getCurLoc() << "동" << endl;
+	}
+	int getJobs() {
+		return count;
+	}
+	int getCurLoc() {
+		return curLoc;
+	}
+};
+
+int main() {
+	int jobs[6];
+	cout << "택배를 픽업할 동을 요청 순서대로 6개 입력>>";
+	for (int i = 0; i < 6; i++)
+		cin >> jobs[i];
+	Schedule fcfs(10, jobs, 6);
+	fcfs.run();
+}
---
+++ 자료구조/실습/기말고사/FinalExam/test/main.c
@@ -0,0 +1,55 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+typedef int element;
+
+typedef struct Stack {
+	element data;
+	struct Stack* link;
+}Stack;
+
+typedef struct {
+	Stack* top;
+}LinkedStack;
+
+void init(LinkedStack* s) {
+	s->top = NULL;
+}
+
+int is_empty(LinkedStack* s) {
+	return s->top == NULL;
+}
+
+void push(LinkedStack* s, element item) {
+	Stack* node = (Stack*)malloc(sizeof(Stack));
+	node->data = item;
+	node->link = s->top;
+	s->top = node;
+	
+}
+
+element pop(LinkedStack* s) {
+	Stack* node = s->top;
+	element item = node->data;
+	s->top = node->link;
+	free(node);
+	return item;
+}
+
+void print_stack(LinkedStack* s) {
+	for (Stack* p = s->top; p != NULL; p = p->link) {
+		printf("%d->", p->data);
+	}
+	printf("NULL \n");
+}
+
+int main() {
+	LinkedStack head;
+	init(&head);
+	push(&head, 1); print_stack(&head);
+	push(&head, 2); print_stack(&head);
+	push(&head, 3); print_stack(&head);
+	pop(&head); print_stack(&head);
+	pop(&head); print_stack(&head);
+	pop(&head); print_stack(&head);
+}
