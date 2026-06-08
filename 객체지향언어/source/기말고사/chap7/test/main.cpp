44942cd0592d47e0c5d6eb9c94e5ff85b23dbe00
hansung6326 <hansung6326@gmail.com>
2026-06-09 04:48:18 +0900

    링크 트리 표현법 프로그램

--- 객체지향언어/source/기말고사/chap7/test/main.cpp
+++ 객체지향언어/source/기말고사/chap7/test/main.cpp
@@ -1,35 +1,27 @@
 ﻿#include <iostream>
-#include <vector>
+#include <fstream>
 #include <string>
 using namespace std;
 
 int main() {
-	string grades[] = { "A+", "A", "B+", "B", "C+", "C", "D+", "D", "F" };
-	double scores[] = { 4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0 };
-	vector<string> v;
+	ifstream fin("c:\\windows\\system.ini", ios::in | ios::binary);
+	if (!fin) {
+		exit(1);
+	}
+	ofstream fout("c:\\temp\\system.txt", ios::out | ios::binary);
+	if (!fout) {
+		exit(1);
+	}
+	fin.seekg(0, ios::end);
+	int size = fin.tellg();
 
-	string n;
-	for (int i = 0; i < 6; i++) {
-		cin >> n;
-		v.push_back(n);
+	int c;
+	for (int i = 0; i < size; i++) {
+		fin.seekg(size - i - 1, ios::beg);
+		c = fin.get();
+		fout.put(c);
 	}
 
-	double sum = 0;
-	for (int i = 0; i < v.size(); i++) {
-		int j;
-		for (j = 0; j < 9; j++) {
-			if (v[i] == grades[j]) {
-				sum += scores[j];
-				break;
-			}
-			
-		}
-		if (j == 9) {
-			exit(1);
-		}
-	}
-
-	cout << sum / v.size();
-
+	fin.close();
+	fout.close();
 }
-
