194cfcedca74a23bac6765327cb2139385e52efc
hansung6326 <hansung6326@gmail.com>
2026-06-08 13:10:32 +0900

    PL14 주차 template 실습

---
+++ PL/code_practice/PL14/polym/polym/main.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+template<class T>
+void swap_function(T& x, T& y) {
+	T t;
+	t = x;
+	x = y;
+	y = t;
+}
+
+int main() {
+	int a, b;
+	double c, d;
+	char x, y;
+
+	a = 10;
+	b = 20;
+
+	cout << "a= " << a << ", b= " << b << endl;
+	swap_function(a, b);
+	cout << "a= " << a << ", b= " << b << endl;
+
+	c = 10.5;
+	d = 20.4;
+
+	cout << "c= " << c << ", d= " << d << endl;
+	swap_function(c, d);
+	cout << "c= " << c << ", d= " << d << endl;
+
+	x = 'X';
+	y = 'Y';
+
+	cout << "x= " << x << ", y= " << y << endl;
+	swap_function(x, y);
+	cout << "x= " << x << ", y= " << y << endl;
+
+}
